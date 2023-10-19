bb2628281067cc7aeeb73b897e2bd1e1c7de4124
Triveni Danda <triveni.danda@nordicsemi.no>
2023-10-19 17:42:03 +0530

    samples: wifi: raw_tx_packet: Add channel setting functionality
    Add functionality to program the channel for raw tx packet
    transmission in idle mode.
    
    Signed-off-by: Triveni Danda <triveni.danda@nordicsemi.no>

--- samples/wifi/raw_tx_packet/src/main.c
+++ samples/wifi/raw_tx_packet/src/main.c
@@ -224,6 +224,51 @@ static int wifi_connect(void)
 }
 #endif
 
+#ifdef CONFIG_RAW_TX_PACKET_SAMPLE_IDLE_MODE
+static void wifi_set_channel(void)
+{
+	struct net_if *iface;
+	struct wifi_channel_info channel_info = {0};
+	int ret;
+
+	channel_info.oper = WIFI_MGMT_SET;
+
+	if (channel_info.if_index == 0) {
+		iface = net_if_get_first_wifi();
+		if (iface == NULL) {
+			LOG_ERR("Cannot find the default wifi interface");
+			return;
+		}
+		channel_info.if_index = net_if_get_by_iface(iface);
+	} else {
+		iface = net_if_get_by_index(channel_info.if_index);
+		if (iface == NULL) {
+			LOG_ERR("Cannot find interface for if_index %d",
+					      channel_info.if_index);
+			return;
+		}
+	}
+
+	if (channel_info.oper == WIFI_MGMT_SET) {
+		channel_info.channel = CONFIG_RAW_TX_PACKET_SAMPLE_CHANNEL;
+		if ((channel_info.channel < WIFI_CHANNEL_MIN) ||
+			   (channel_info.channel > WIFI_CHANNEL_MAX)) {
+			LOG_ERR("Invalid channel number. Range is (1-233)");
+			return;
+		}
+	}
+
+	ret = net_mgmt(NET_REQUEST_WIFI_CHANNEL, iface,
+			&channel_info, sizeof(channel_info));
+	if (ret) {
+		LOG_ERR(" Channel setting failed %d\n", ret);
+			return;
+	}
+
+	LOG_INF("Wi-Fi channel set to %d", channel_info.channel);
+}
+#endif
+
 static void wifi_set_mode(void)
 {
 	int ret;
@@ -297,6 +342,8 @@ int main(void)
 			LOG_ERR("Connection Timed Out");
 		}
 	}
+#else
+	wifi_set_channel();
 #endif
 
 	return 0;
